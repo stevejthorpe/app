febb681d1a00e60be1d5d82f27b404029f634ca8
stevejthorpe <stevejthorpe@users.noreply.github.com>
2020-04-14 10:49:22 +0000

    deploy: 07c177287825ede999bd64cd1539ec12753d405d

---
+++ precache-manifest.30138fa40a98fdf97a06f02a708953a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a91b1755584f420463cb0134128c98",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "925b35a65150ad0c9b47",
+    "url": "/app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5602b57f09e24bcc9896",
+    "url": "/app/static/js/2.b4ba21af.chunk.js"
+  },
+  {
+    "revision": "cc7f4817303dc5ae24c42d02bfaefb45",
+    "url": "/app/static/js/2.b4ba21af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925b35a65150ad0c9b47",
+    "url": "/app/static/js/main.620ae433.chunk.js"
+  },
+  {
+    "revision": "174fb68280a43e54e3e3",
+    "url": "/app/static/js/runtime-main.ee29eaf4.js"
+  },
+  {
+    "revision": "2a1edab4bd3683c406b4c45937c45792",
+    "url": "/app/static/media/LanguagesIcon.2a1edab4.svg"
+  },
+  {
+    "revision": "92c6d77ac5adaccb87863213de908c81",
+    "url": "/app/static/media/Logoshort.92c6d77a.svg"
+  },
+  {
+    "revision": "6eab937fda7bcf498041a3ec5261d203",
+    "url": "/app/static/media/POIIcon.6eab937f.svg"
+  },
+  {
+    "revision": "19331d5d719ea76865f4657afb432576",
+    "url": "/app/static/media/QuestionairePicture.19331d5d.jpg"
+  }
+]);
